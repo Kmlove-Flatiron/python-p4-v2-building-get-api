2cad087bdfead02ce9b934d4663c47dbce94fe19
Kim Love <kimberlymlove15@gmail.com>
2023-10-06 16:55:48 -0400

    Test

--- server/models.py
+++ server/models.py
@@ -5,6 +5,8 @@ from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
+test = "test"
+
 metadata = MetaData(
     naming_convention={
         "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
